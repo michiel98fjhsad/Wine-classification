6fb3da4d5d40b7ce60e298a4f1becbbb61d0887a
michiel98fjhsad <94781488+michiel98fjhsad@users.noreply.github.com>
2022-01-25 16:09:08 +0100

    deleted libraries

--- Wine-classification.R
+++ Wine-classification.R
@@ -1,7 +1,6 @@
 set.seed(42)
-library(caret)
-library(rpart)
 library(randomForest)
+library(ggplot2)
 data <- read.csv("C:/Users/michi/Downloads/wine.data")
 names <- cbind("Class", "Alcohol", "Malic acid",
                "Ash",
@@ -17,7 +16,6 @@ names <- cbind("Class", "Alcohol", "Malic acid",
                "Proline")
 colnames(data) <- names
 wine.data <- data.frame(data)
-lm(Class ~ ., data = wine.data) 
 ggplot(wine.data, aes(x=Alcohol)) + geom_histogram()
 
 wine.data$Class <- as.factor(wine.data$Class)
